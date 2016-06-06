b3eb8bde7b3134eb4675768e6af708fe91c45fec
Cory Feitelson <cfeitelson@roovy.com>
2016-06-05 17:53:32 -0700

    hooked up list activity
    Signed-off-by: Cory Feitelson <cfeitelson@roovy.com>

--- app/src/main/java/com/hackmobile/hackmobile/MainActivity.java
+++ app/src/main/java/com/hackmobile/hackmobile/MainActivity.java
@@ -1,13 +1,31 @@
 package com.hackmobile.hackmobile;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button launch_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize button
+        launch_button = (Button)findViewById(R.id.hello_world_launch_button);
+        // setup button's click action
+        launch_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+
     }
 }
